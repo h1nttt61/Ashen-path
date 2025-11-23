57cf49004f7c65a47c700b24e44267d5710bb62e
Лопушко Максим <maxelonir@yandex.ru>
2025-11-23 19:05:18 +0300

    Fixed compilation errors in scripts

--- Assets/Scripts/Weapons/Sword/Sword.cs
+++ Assets/Scripts/Weapons/Sword/Sword.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Security.Authentication.ExtendedProtection;
 using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
+//using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 public class Sword : MonoBehaviour
 {
