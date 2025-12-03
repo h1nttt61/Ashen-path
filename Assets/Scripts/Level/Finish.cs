22c98f455334de6042a00a0091a411641e9bc36a
Kirill Sidorov <kir.sid06@gmail.com>
2025-12-03 23:37:20 +0300

    Added the first transition between scenes

--- Assets/Scripts/Level/Finish.cs
+++ Assets/Scripts/Level/Finish.cs
@@ -3,11 +3,34 @@ using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
+    [SerializeField] private string playerTag = "Player";
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.CompareTag(playerTag))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            LoadNextScene();
         }
+    }
+
+    private void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.Log("Это последняя сцена!");
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log($"Объект в триггере: {collision.gameObject.name}, тег: {collision.tag}");
     }
 }
