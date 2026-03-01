36ee5d4374edae7e6bd20b8001e98a67ab3a7ede
h1nttt61 <Lyl1.32@yandex.ru>
2026-03-02 00:47:13 +0300

    BUG WITH LOCATION NUMBER 1

--- Assets/Scripts/scr.cs
+++ Assets/Scripts/scr.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using UnityEngine;
 
 public class scr : MonoBehaviour
@@ -15,21 +14,3 @@ public class scr : MonoBehaviour
         
     }
 }
-=======
-using UnityEngine;
-
-public class scr : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
->>>>>>> 829128e (Colliders fix)
